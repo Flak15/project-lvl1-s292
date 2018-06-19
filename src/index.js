90fd49e6de3191a05bc091a06894a270982e45a1
root <flak160@gmail.com>
2018-06-19 10:35:23 +0300

    ltr chk

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
 export const plName = () => {
-  const  playerName = readlineSync.question('May I have your name? ');
+  const playerName = readlineSync.question('May I have your name? ');
   return `Hello, ${playerName}!`;
-}
+};
