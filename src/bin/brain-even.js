268a4dc77c546ecc5ff3e1677f9f073a49bc8741
root <flak160@gmail.com>
2018-06-20 22:15:05 +0300

    lint

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,11 +1,9 @@
 #!/usr/bin/env node
 
-import {game} from '..';
-import plName from '..';
+import { game, plName } from '..';
+
 
 console.log('Welcome to the Brain Games!\nAnswer "yes" if number even otherwise answer "no"\n');
-
-
 
 const name = plName();
 const rounds = 3;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-const plName = () => {
+export const plName = () => {
   const playerName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${playerName}!`);
   return playerName;
@@ -10,19 +10,16 @@ const question = () => {
   const num = Math.floor(Math.random() * 100);
   console.log(`Question: ${num}`);
   const plAns = readlineSync.question('Your answer: ');
-  const ans = {yes: 1 , no: 0};
-  if (num % 2 + ans[plAns] === 1) {
+  const ans = { yes: 1, no: 0 };
+  if ((num % 2) + ans[plAns] === 1) {
     console.log('Correct!');
     return true;
   }
-  else {
-    console.log(`'${plAns}' is wrong answer :(. Correct answer was ${num % 2 === 0 ? "'yes'" : "'no'"}`);
-    return false;
-  }
-}
+  console.log(`'${plAns}' is wrong answer :(. Correct answer was ${num % 2 === 0 ? "'yes'" : "'no'"}`);
+  return false;
+};
 
 export const game = (name, rounds) => {
-  if (rounds === 0) return console.log(`Congratulations,  ${name}!`);
+  if (rounds === 0) return console.log(`Congratulations, ${name}!`);
   return question() ? game(name, rounds - 1) : console.log(`Lets try again, ${name}!`);
-}
-export default plName;
+};
